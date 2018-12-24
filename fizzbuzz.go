853eff482ee6ee1f282f0da0ad1a4ec95a15d017
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-24 23:55:49 +0700

    refactor

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,12 +1,11 @@
 package fizzbuzz
 
+import "strconv"
+
 // FizzBuzz input integer and return string
 func FizzBuzz(x int) string {
-	if x == 2 {
-		return "2"
-	}
 	if x == 3 {
 		return "fizz"
 	}
-	return "1"
+	return strconv.Itoa(x)
 }
