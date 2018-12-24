5e18f4421e8c8c65075315190c7c48277a0901a6
Thawatchai Singngam <merxer.io@gmail.com>
2018-12-24 23:51:16 +0700

    when input 2 return string "2"

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,5 +2,8 @@ package fizzbuzz
 
 // FizzBuzz input integer and return string
 func FizzBuzz(x int) string {
+	if x == 2 {
+		return "2"
+	}
 	return "1"
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -9,3 +9,11 @@ func TestWhenInput_1_want_1(t *testing.T) {
 		t.Errorf("Got %s, expected %s", fb, expected)
 	}
 }
+
+func TestWhenInput_2_want_2(t *testing.T) {
+	fb := FizzBuzz(2)
+	expected := "2"
+	if fb != expected {
+		t.Errorf("Got %s, expected %s", fb, expected)
+	}
+}
